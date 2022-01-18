234669ab348007ef5c4f5243d988f82a832104be
Katherine Smith <katherine.lo.smith@gmail.com>
2022-01-18 10:02:14 -0800

    Updated content of Home page and About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,29 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { aboutQuote } from './page.module.css'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About">
-      <p>This is the about section</p>
+      <p>
+        Matt takes bird photos using a Canon EOS R5 camera with a Canon RF 100-500mm F4.5-7.1 lens.
+        He has been taking bird photos since November 2020.
+        His current favorite bird is the great blue heron because they fly so slowly and are easy to capture.
+        He hopes to take a photo of a great eared nightjar one day.
+      </p>
+      <p className={aboutQuote}>
+        “Ugh I left it in crop mode.” -Matt
+      </p>
+      <p>
+        Katherine takes bird photos using a Sony Alpha A6600 camera with a Sony Alpha 70-350mm F4.5-6.3 lens.
+        She has been taking bird photos since August 2021.
+        Her current favorite bird is the snowy owl because snowy owl.
+        She has only ever seen one at the Woodland Park Zoo.
+        She hopes to take a photo of a snowy owl outside of the zoo one day.
+      </p>
+      <p className={aboutQuote}>
+        "My shutter speed is too slow." -Katherine
+      </p>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { homeImage } from './page.module.css'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
-      <p>I'm still making this by following the Gatsby Tutorial</p>
+      <p>
+        Welcome to the bird photography site of Matt and Katherine Smith.
+        Here you will find bird photos taken either in our backyard in Bothell, WA or out and about across the US.
+        We hope you enjoy seeing our bird photos!
+      </p>
+      <img
+        className={homeImage}
+        src="https://bird-photography.s3.us-west-2.amazonaws.com/home-photo.jpg"
+        alt="Katherine, Matt, Hero, and Luna in the Cascades"
+      />
     </Layout>
   )
 }
